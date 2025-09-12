204d0b356bb66d44b4a23e13f5c991a48d6ec311
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-09-12 13:58:08 +0900

    log追加

--- lambdas/answerQuiz/src/index.ts
+++ lambdas/answerQuiz/src/index.ts
@@ -21,6 +21,10 @@ export const handler:Handler = wrap(async(event)=>{
         if(!process.env.MYSQL_HOST || !process.env.MYSQL_USER || !process.env.MYSQL_PASSWORD || !process.env.MYSQL_DATABASE){
             throw internal("Missing required environment variables");
         }
+        console.log("MYSQL_HOST", process.env.MYSQL_HOST);
+        console.log("MYSQL_USER", process.env.MYSQL_USER);
+        console.log("MYSQL_PASSWORD", process.env.MYSQL_PASSWORD);
+        console.log("MYSQL_DATABASE", process.env.MYSQL_DATABASE);
         conn = await mysql.createConnection({
             host: process.env.MYSQL_HOST,
             user: process.env.MYSQL_USER,
